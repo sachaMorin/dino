479a3d2e21948e2f01477669b35fcd85f810beee
sachaMorin <morin.sacha@gmail.com>
2021-12-08 13:56:52 -0500

    Make torch KNN predictor return actual predictions
    And not simply the accuracy.

--- eval_knn.py
+++ eval_knn.py
@@ -141,9 +141,10 @@ def extract_features(model, data_loader, use_cuda=True, multiscale=False):
 
 @torch.no_grad()
 def knn_classifier(train_features, train_labels, test_features, test_labels, k, T, num_classes=1000):
+    pred = list()
     top1, top5, total = 0.0, 0.0, 0
     train_features = train_features.t()
-    num_test_images, num_chunks = test_labels.shape[0], 100
+    num_test_images, num_chunks = test_labels.shape[0], 2000
     imgs_per_chunk = num_test_images // num_chunks
     retrieval_one_hot = torch.zeros(k, num_classes).to(train_features.device)
     for idx in range(0, num_test_images, imgs_per_chunk):
@@ -158,7 +159,7 @@ def knn_classifier(train_features, train_labels, test_features, test_labels, k, 
         similarity = torch.mm(features, train_features)
         distances, indices = similarity.topk(k, largest=True, sorted=True)
         candidates = train_labels.view(1, -1).expand(batch_size, -1)
-        retrieved_neighbors = torch.gather(candidates, 1, indices)
+        retrieved_neighbors = torch.gather(candidates, 1, indices).type(torch.int64)
 
         retrieval_one_hot.resize_(batch_size * k, num_classes).zero_()
         retrieval_one_hot.scatter_(1, retrieved_neighbors.view(-1, 1), 1)
@@ -171,15 +172,17 @@ def knn_classifier(train_features, train_labels, test_features, test_labels, k, 
             1,
         )
         _, predictions = probs.sort(1, True)
+        pred.append(predictions[:, 0])
 
         # find the predictions that match the target
         correct = predictions.eq(targets.data.view(-1, 1))
         top1 = top1 + correct.narrow(1, 0, 1).sum().item()
         top5 = top5 + correct.narrow(1, 0, min(5, k)).sum().item()  # top5 does not make sense if k < 5
         total += targets.size(0)
+
     top1 = top1 * 100.0 / total
     top5 = top5 * 100.0 / total
-    return top1, top5
+    return top1, top5, torch.cat(pred).cpu().numpy()
 
 
 class ReturnIndexDataset(datasets.ImageFolder):
