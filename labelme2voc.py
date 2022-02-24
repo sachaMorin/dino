3e8e4c719ed67ac9bc377149a545bfe940f95602
sachaMorin <morin.sacha@gmail.com>
2022-02-23 19:58:14 -0500

    Comet logging

--- labelme2voc.py
+++ labelme2voc.py
@@ -16,6 +16,22 @@ import numpy as np
 
 import labelme
 
+def parse_class_names(path):
+    class_names = []
+    class_name_to_id = {}
+    for i, line in enumerate(open(path).readlines()):
+        class_id = i - 1  # starts with -1
+        class_name = line.strip()
+        class_name_to_id[class_name] = class_id
+        if class_id == -1:
+            assert class_name == "__ignore__"
+            continue
+        elif class_id == 0:
+            assert class_name == "_background_"
+        class_names.append(class_name)
+    class_names = tuple(class_names)
+    return class_names
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -42,19 +58,7 @@ def main():
         )
     print("Creating dataset:", args.output_dir)
 
-    class_names = []
-    class_name_to_id = {}
-    for i, line in enumerate(open(args.labels).readlines()):
-        class_id = i - 1  # starts with -1
-        class_name = line.strip()
-        class_name_to_id[class_name] = class_id
-        if class_id == -1:
-            assert class_name == "__ignore__"
-            continue
-        elif class_id == 0:
-            assert class_name == "_background_"
-        class_names.append(class_name)
-    class_names = tuple(class_names)
+    class_names = parse_class_names(args.labels)
     print("class_names:", class_names)
     out_class_names_file = osp.join(args.output_dir, "class_names.txt")
     with open(out_class_names_file, "w") as f:
--- run_experiment.py
+++ run_experiment.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 """Minimal script to fit DINO backbone for Duckietown segmentation."""
+import comet_ml
 import os
 import pandas as pd
 import argparse
@@ -7,6 +8,8 @@ import argparse
 import numpy as np
 import torch
 from torch.optim import Adam, AdamW, SGD
+from pytorch_lightning.loggers import CometLogger
+from labelme2voc import parse_class_names
 
 from dt_segmentation import DINOSeg
 import warnings
@@ -15,7 +18,7 @@ warnings.filterwarnings("ignore", category=UserWarning)
 
 
 def run_experiment(data_path, write_path, batch_size, epochs, learning_rate, patience, n_blocks, finetune, seed,
-                   ck_file_name=None):
+                   ck_file_name=None, comet_tag=None):
     """Fit coarse segmentation model on Duckietown data. We use DINO as the backbone and output a prediction for
     every 8x8 token in the image.
 
@@ -42,7 +45,8 @@ def run_experiment(data_path, write_path, batch_size, epochs, learning_rate, pat
         Random seed.
     ck_file_name : str, default:None
         Name of the checkpoint and prediction file names.
-
+    comet_tag : str, default:None
+        If a comet tag is provided we log the experiments to comet with the provided tag.
     """
     # Seed experiments
     np.random.seed(seed)
@@ -53,12 +57,25 @@ def run_experiment(data_path, write_path, batch_size, epochs, learning_rate, pat
     val_path = os.path.join(data_path, 'dt_real_voc_val')
     test_path = os.path.join(data_path, 'dt_real_voc_test')
 
+    # Initialize comet logger if requested
+    if comet_tag is not None:
+        comet_logger = CometLogger(
+            api_key=os.environ.get("COMET_API_KEY"),
+            project_name="duck"
+        )
+        comet_logger.experiment.add_tag(comet_tag)
+    else:
+        comet_logger = None
+
+    # Get class names and length
+    class_names = parse_class_names(os.path.join(data_path, 'labels.txt'))
+
     # Number of transformer blocks to use in the backbone
     # MLP Head
     mlp_frozen = DINOSeg(head='mlp', train_path=train_path, val_path=val_path, test_path=test_path,
-                         write_path=write_path,
+                         write_path=write_path, n_classes=len(class_names), class_names=class_names,
                          freeze_backbone=True, optimizer=Adam, lr=learning_rate, batch_size=batch_size,
-                         n_blocks=n_blocks, max_epochs=epochs, patience=patience)
+                         n_blocks=n_blocks, max_epochs=epochs, patience=patience, comet_logger=comet_logger)
 
     if ck_file_name is None:
         # Generate a checkpoint file name
@@ -77,6 +94,16 @@ def run_experiment(data_path, write_path, batch_size, epochs, learning_rate, pat
 
     # Fine tune, we don't do this for now
     if finetune:
+        # Initialize comet logger if requested
+        if comet_tag is not None:
+            comet_logger = CometLogger(
+                api_key=os.environ.get("COMET_API_KEY"),
+                project_name="duck"
+            )
+            comet_logger.experiment.add_tag(comet_tag)
+        else:
+            comet_logger = None
+
         print("\n Finetuning the previous model...")
         # MLP Head + Fine tune backbone
         # Only finetune with fewer than 5 blocks, might otherwise run out of GPU RAM
@@ -84,7 +111,10 @@ def run_experiment(data_path, write_path, batch_size, epochs, learning_rate, pat
         mlp_dino.freeze_backbone = False
         mlp_dino.optimizer = AdamW
         mlp_dino.lr /= 2  # Lower the learning rate for fine-tuning
-        ck_file_name = ck_file_name[:-5] + '_finetuned'
+
+        # Add new comet logger
+        mlp_dino.comet_logger = comet_logger
+        ck_file_name = ck_file_name + '_finetuned'
         mlp_dino.fit(ck_file_name)
         results["pred_mlp_finetuned"] = mlp_dino.predict_dl(mlp_dino.test_dataloader())
 
@@ -106,6 +136,9 @@ if __name__ == '__main__':
     parser.add_argument("--finetune",
                         help="Finetune DINO backbone after an initial training phase with a frozen backbone",
                         required=False, default=False, type=bool)
+    parser.add_argument("--comet_tag",
+                        help=" If a comet tag is provided we log the experiments to comet with the provided tag.",
+                        required=False, default=None, type=str)
     parser.add_argument("--seed", help="random_seed", required=False, default=42, type=int)
     args = parser.parse_args()
 
