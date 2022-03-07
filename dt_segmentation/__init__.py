5159b0c1d336900a622c862d36a1fd649657cc60
sachaMorin <morin.sacha@gmail.com>
2022-03-07 13:59:46 -0500

    Improve resoluton handling

--- dt_segmentation/__init__.py
+++ dt_segmentation/__init__.py
@@ -1,2 +1,2 @@
-from dt_segmentation.src.pl_torch_modules import DINOSeg
+from dt_segmentation.src.pl_torch_modules import DINOSeg, DuckieSegDataset
 from dt_segmentation.src.dt_utils import parse_class_names
